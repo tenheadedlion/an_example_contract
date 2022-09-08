62e3d065e4bc3f9d9700db8022e10c9594bb4cd5
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-09-08 18:26:26 +0800

    safely add some functions

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod agency {
     use super::pink;
     use ink_env;
     use ink_env::debug_println;
-    
+
     use ink_prelude::format;
     use ink_prelude::vec;
     use ink_prelude::{
@@ -30,8 +30,9 @@ mod agency {
         registered_agents: Mapping<AccountId, Record>,
     }
 
-    #[derive(Encode, Decode, Debug, PartialEq, Eq, Clone)]
-    #[derive(SpreadLayout, PackedLayout, SpreadAllocate)]
+    #[derive(
+        Encode, Decode, Debug, PartialEq, Eq, Clone, SpreadLayout, PackedLayout, SpreadAllocate,
+    )]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub struct Record {
         name: String,
@@ -40,7 +41,8 @@ mod agency {
     #[derive(Encode, Decode, Debug, PartialEq, Eq, Copy, Clone)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
-        NoPermission,
+        PermissionDenial,
+        NoSuchTarget
     }
 
     pub type Result<T> = core::result::Result<T, Error>;
@@ -51,23 +53,47 @@ mod agency {
             let admin = Self::env().caller();
             ink_lang::utils::initialize_contract(|this: &mut Self| {
                 this.admin = admin;
+                this.registered_agents.insert(admin, &Record {
+                    name: "admin".to_string()
+                });
             })
         }
 
+        /// Enrolls a new agent
+        ///
+        /// Admin permission is require
         #[ink(message)]
         pub fn enroll(&mut self, agent_id: AccountId, agent_name: String) -> Result<()> {
             let caller = Self::env().caller();
             if caller != self.admin {
-                return Err(Error::NoPermission);
+                return Err(Error::PermissionDenial);
             }
             self.registered_agents
                 .insert(agent_id, &Record { name: agent_name });
             Ok(())
         }
 
+        /// Get the name of a colleague
+        /// 
+        /// The caller must be a member of the agency
         #[ink(message)]
-        pub fn seal_name(&mut self, name: String) {
-            self.name = name;
+        pub fn name_of(&self, agent_id: AccountId) -> Result<String> {
+            let caller = Self::env().caller();
+            if !self.registered_agents.contains(caller) {
+                return Err(Error::PermissionDenial);
+            }
+            if !self.registered_agents.contains(agent_id) {
+                return Err(Error::NoSuchTarget);
+            }
+
+            Ok(self.registered_agents.get(agent_id).unwrap().name)
+        }
+
+        #[ink(message)]
+        pub fn who_is_admin(&self) -> AccountId {
+            self.admin
         }
     }
+    
+    
 }
